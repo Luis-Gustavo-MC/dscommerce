5ec9e402e1a253ed2f5e2ed801074e4fab1cab8c
Luis Gustavo <lgmc120@gmail.com>
2025-03-06 15:04:49 -0300

    Order and ManyToOne and OneToMany

--- src/main/java/com/LSG/dscommerce/entities/User.java
+++ src/main/java/com/LSG/dscommerce/entities/User.java
@@ -3,6 +3,9 @@ package com.LSG.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "tb_user")
 public class User {
@@ -14,6 +17,9 @@ public class User {
     private String phone;
     private LocalDate birthDate;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<Order>();
 
     public User(){}
 
@@ -72,5 +78,9 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 }
