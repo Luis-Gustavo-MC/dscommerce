5fe4cc56ec195c3fb1acf2bf585a79962d722cd2
Luis Gustavo <lgmc120@gmail.com>
2025-03-12 21:08:41 -0300

    PUT product

--- src/main/java/com/LSG/dscommerce/controllers/ProductController.java
+++ src/main/java/com/LSG/dscommerce/controllers/ProductController.java
@@ -2,7 +2,6 @@ package com.LSG.dscommerce.controllers;
 
 import com.LSG.dscommerce.dto.ProductDTO;
 import com.LSG.dscommerce.services.ProductService;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -31,7 +30,13 @@ public class ProductController {
     @PostMapping
     public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto){
         dto = service.insert(dto);
+        //Padrao de resposta 201
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
+    }
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO>  Update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+        dto = service.update(dto,id);
+        return ResponseEntity.ok(dto);
     }
 }
--- src/main/java/com/LSG/dscommerce/services/ProductService.java
+++ src/main/java/com/LSG/dscommerce/services/ProductService.java
@@ -3,13 +3,13 @@ package com.LSG.dscommerce.services;
 import com.LSG.dscommerce.dto.ProductDTO;
 import com.LSG.dscommerce.entities.Product;
 import com.LSG.dscommerce.repositories.ProductRepository;
+import jakarta.persistence.Entity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -31,14 +31,30 @@ public class ProductService {
     }
     @Transactional
     public ProductDTO insert(ProductDTO dto){
+
         Product entity = new Product();
-        entity.setName(dto.getName());
-        entity.setDescripton(entity.getDescripton());
-        entity.setImgURL(entity.getImgURL());
-        entity.setPrice(entity.getPrice());
+
+        copyDtoToEntity(dto,entity);
 
         entity = repository.save(entity);
 
         return new ProductDTO(entity);
+    }
+    @Transactional
+    public ProductDTO update(ProductDTO dto, Long id){
+        Product entity = repository.getReferenceById(id);
+
+        copyDtoToEntity(dto,entity);
+
+        entity = repository.save(entity);
+
+        return new ProductDTO(entity);
+    }
+
+    private void copyDtoToEntity(ProductDTO dto, Product entity) {
+        entity.setName(dto.getName());
+        entity.setDescripton(entity.getDescripton());
+        entity.setImgURL(entity.getImgURL());
+        entity.setPrice(entity.getPrice());
     }
 }
